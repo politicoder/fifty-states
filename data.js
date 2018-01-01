10a1caf09557ea8e96104fafae487a90cb6a9d08
Lee 🎩 <lee.moran@engagedc.com>
2018-01-01 12:04:14 -0500

    getState() method

---
+++ data.js
@@ -0,0 +1,410 @@
+module.exports.states = [
+    {
+        name: 'Alabama',
+        abbreviation: 'AL',
+        ap: 'Ala.',
+        capital: 'Montgomery',
+        population: 4833722,
+        size: 52420
+    },
+    {
+        name: 'Alaska',
+        abbreviation: 'AK',
+        ap: 'Alaska',
+        capital: 'Juneau',
+        population: 735132,
+        size: 665384
+    },
+    {
+        name: 'Arizona',
+        abbreviation: 'AZ',
+        ap: 'Az.',
+        capital: 'Phoenix',
+        population: 6626624,
+        size: 113990
+    },
+    {
+        name: 'Arkansas',
+        abbreviation: 'AR',
+        ap: 'Ark.',
+        capital: 'Little Rock',
+        population: 2959373,
+        size: 53178
+    },
+    {
+        name: 'California',
+        abbreviation: 'CA',
+        ap: 'Calif.',
+        capital: 'Sacramento',
+        population: 38332521,
+        size: 163694
+    },
+    {
+        name: 'Colorado',
+        abbreviation: 'CO',
+        ap: 'Colo.',
+        capital: 'Denver',
+        population: 5268367,
+        size: 104093
+    },
+    {
+        name:'Connecticut',
+        abbreviation: 'CT',
+        ap: 'Conn.',
+        capital: 'Hartford',
+        population: 3596080,
+        size: 5543
+    },
+    {
+        name: 'Washington, D.C.',
+        abbreviation: 'DC',
+        ap: 'D.C.',
+        capital: 'Washington, D.C.',
+        population: 646449,
+        size: 68
+    },
+    {
+        name: 'Delaware',
+        abbreviation: 'DE',
+        ap: 'Del.',
+        capital: 'Dover',
+        population: 925749,
+        size: 2489
+    },
+    {
+        name: 'Florida',
+        abbreviation: 'FL',
+        ap: 'Fla',
+        capital: 'Tallahassee',
+        population: 19552860,
+        size: 65758
+    },
+    {
+        name: 'Georgia',
+        abbreviation: 'GA',
+        ap: 'Ga.',
+        capital: 'Atlanta',
+        population: 9992167,
+        size: 59425
+    },
+    {
+        name: 'Hawaii',
+        abbreviation: 'HI',
+        ap: 'Hawaii',
+        capital: 'Honolulu',
+        population: 1404054,
+        size: 10932
+    },
+    {
+        name: 'Idaho',
+        abbreviation: 'ID',
+        ap: 'Idaho',
+        capital: 'Boise',
+        population: 1612136,
+        size: 83569
+    },
+    {
+        name: 'Illinois',
+        abbreviation: 'IL',
+        ap: 'Ill.',
+        capital: 'Springfield',
+        population: 12882135,
+        size: 57914
+    },
+    {
+        name: 'Indiana',
+        abbreviation: 'IN',
+        ap: 'Ind.',
+        capital: 'Indianapolis',
+        population: 6570902,
+        size: 36420
+    },
+    {
+        name: 'Iowa',
+        abbreviation: 'IA',
+        ap: 'Iowa',
+        capital: 'Des Moines',
+        population: 3090416,
+        size: 56273
+    },
+    {
+        name: 'Kansas',
+        abbreviation: 'KS',
+        ap: 'Kan.',
+        capital: 'Topeka',
+        population: 2893957,
+        size: 82278
+    },
+    {
+        name: 'Kentucky',
+        abbreviation: 'KY',
+        ap: 'Ky.',
+        capital: 'Frankfort',
+        population: 4395295,
+        size: 40408
+    },
+    {
+        name: 'Louisiana',
+        abbreviation: 'LA',
+        ap: 'La.',
+        capital: 'Baton Rouge',
+        population: 4625470,
+        size: 52378
+    },
+    {
+        name: 'Maine',
+        abbreviation: 'ME',
+        ap: 'Maine',
+        capital: 'Augusta',
+        population: 1328302,
+        size: 35380
+    },
+    {
+        name: 'Maryland',
+        abbreviation: 'MD',
+        ap: 'Md.',
+        capital: 'Annapolis',
+        population: 5928814,
+        size: 12406
+    },
+    {
+        name: 'Massachusetts',
+        abbreviation: 'MA',
+        ap: 'Mass.',
+        capital: 'Boston',
+        population: 6692824,
+        size: 10554
+    },
+    {
+        name: 'Michigan',
+        abbreviation: 'MI',
+        ap: 'Mich.',
+        capital: 'Lansing',
+        population: 9895622,
+        size: 96714
+    },
+    {
+        name: 'Minnesota',
+        abbreviation: 'MN',
+        ap: 'Mich.',
+        capital: 'St. Paul',
+        population: 5420380,
+        size: 86936
+    },
+    {
+        name: 'Mississippi',
+        abbreviation: 'MS',
+        ap: 'Miss.',
+        capital: 'Jackson',
+        population: 2991207,
+        size: 48432
+    },
+    {
+        name: 'Missouri',
+        abbreviation: 'MO',
+        ap: 'Mo.',
+        capital: 'Jefferson City',
+        population: 6044171,
+        size: 69707
+    },
+    {
+        name: 'Montana',
+        abbreviation: 'MT',
+        ap: 'Mont.',
+        capital: 'Helena',
+        population: 1015165,
+        size: 147040
+    },
+    {
+        name: 'Nebraska',
+        abbreviation: 'NE',
+        ap: 'Neb.',
+        capital: 'Lincoln',
+        population: 1868516,
+        size: 77348
+    },
+    {
+        name: 'Nevada',
+        abbreviation: 'NV',
+        ap: 'Nev.',
+        capital: 'Carson City',
+        population: 2790136,
+        size: 110572
+    },
+    {
+        name: 'New Hampshire',
+        abbreviation: 'NH',
+        ap: 'N.H.',
+        capital: 'Concord',
+        population: 1323459,
+        size: 9349
+    },
+    {
+        name: 'New Jersey',
+        abbreviation: 'NJ',
+        ap: 'N.J.',
+        capital: 'Trenton',
+        population: 8899339,
+        size: 8723
+    },
+    {
+        name: 'New Mexico',
+        abbreviation: 'NM',
+        ap: 'N.M.',
+        capital: 'Santa Fe',
+        population: 2085287,
+        size: 121590
+    },
+    {
+        name: 'New York',
+        abbreviation: 'NY',
+        ap: 'N.Y.',
+        capital: 'Albany',
+        population: 19651127,
+        size: 54555
+    },
+    {
+        name: 'North Carolina',
+        abbreviation: 'NC',
+        ap: 'N.C.',
+        capital: 'Raleigh',
+        population: 9848060,
+        size: 53819
+    },
+    {
+        name: 'North Dakota',
+        abbreviation: 'ND',
+        ap: 'N.D.',
+        capital: 'Bismarck',
+        population: 723393,
+        size: 70698
+    },
+    {
+        name: 'Ohio',
+        abbreviation: 'OH',
+        ap: 'Ohio',
+        capital: 'Columbus',
+        population: 11570808,
+        size: 44826
+    },
+    {
+        name: 'Oklahoma',
+        abbreviation: 'OK',
+        ap: 'Okla.',
+        capital: 'Oklahoma City',
+        population: 3850568,
+        size: 69899
+    },
+    {
+        name: 'Oregon',
+        abbreviation: 'OR',
+        ap: 'Ore.',
+        capital: 'Salem',
+        population: 3930065,
+        size: 98379
+    },
+    {
+        name: 'Pennsylvania',
+        abbreviation: 'PA',
+        ap: 'Pa.',
+        capital: 'Harrisburg',
+        population: 12773801,
+        size: 46054
+    },
+    {
+        name: 'Rhode Island',
+        abbreviation: 'RI',
+        ap: 'R.I.',
+        capital: 'Providence',
+        population: 1051511,
+        size: 1545
+    },
+    {
+        name: 'South Carolina',
+        abbreviation: 'SC',
+        ap: 'S.C.',
+        capital: 'Columbia',
+        population: 4774839,
+        size: 32020
+    },
+    {
+        name: 'South Dakota',
+        abbreviation: 'SD',
+        ap: 'S.D.',
+        capital: 'Pierre',
+        population: 844877,
+        size: 77116
+    },
+    {
+        name: 'Tennessee',
+        abbreviation: 'TN',
+        ap: 'Tenn.',
+        capital: 'Nashville',
+        population: 6495978,
+        size: 42144
+    },
+    {
+        name: 'Texas',
+        abbreviation: 'TX',
+        ap: 'Texas',
+        capital: 'Austin',
+        population: 26448193,
+        size: 268596
+    },
+    {
+        name: 'Utah',
+        abbreviation: 'UT',
+        ap: 'Utah',
+        capital: 'Salt Lake City',
+        population: 2900872,
+        size: 84897
+    },
+    {
+        name: 'Vermont',
+        abbreviation: 'VT',
+        ap: 'Vt.',
+        capital: 'Montpelier',
+        population: 626630,
+        size: 9616
+    },
+    {
+        name: 'Virginia',
+        abbreviation: 'VA',
+        ap: 'Va.',
+        capital: 'Richmond',
+        population: 8260405,
+        size: 42775
+    },
+    {
+        name: 'Washington',
+        abbreviation: 'WA',
+        ap: 'Wash.',
+        capital: 'Olympia',
+        population: 6971406,
+        size: 71298
+    },
+    {
+        name: 'West Virginia',
+        abbreviation: 'WV',
+        ap: 'W.Va.',
+        capital: 'Charleston',
+        population: 1854304,
+        size: 24230
+    },
+    {
+        name: 'Wisconsin',
+        abbreviation: 'WI',
+        ap: 'Wis.',
+        capital: 'Madison',
+        population: 5742713,
+        size: 65496
+    },
+    {
+        name: 'Wyoming',
+        abbreviation: 'WY',
+        ap: 'Wyo.',
+        capital: 'Cheyenne',
+        population: 582658,
+        size: 97813
+    }
+];
